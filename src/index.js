22a032d511dacb7e90639ed4f10843faa432f792
Ricardo João António <ricardojoaoantonio2@gmail.com>
2021-11-26 19:48:53 +0100

    res

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 const express = require("express");
+
+const app = express();
+
+app.get("/", (request, response) => {
+  return response.json({ messagen: "Hello word! Ignite"});
+  console.log("esta bem");
+});
+
+app.listen(3000);
